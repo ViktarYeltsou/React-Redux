ba6a422d52be1cf7599139706c5057550ecdc6a8
ViktarYeltsou <fhctybl.33>
2022-02-03 13:38:59 +0300

    feat: add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://previews.123rf.com/images/ikalvi/ikalvi1712/ikalvi171200434/92428932-workman-or-builder-logo-design-civil-eingineer-sign-construction-and-repairing-vector-logo-design-.jpg"
+        alt="logo"/>
+      header
+    </header>
+  )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt=""/>
+      </div>
+      <div>
+        ava+description
+      </div>
+      <div>
+        My posts
+        <div>
+          new post
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Profile;
