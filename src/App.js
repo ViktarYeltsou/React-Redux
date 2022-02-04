8ed568eabd70fbf79b4a559f895159692daac5d9
ViktarYeltsou <fhctybl.33>
2022-02-04 15:22:18 +0300

    feat: add Routs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Router} from "react-router-dom";
 
-function App() {
+function App(props) {
+
   return (
+
     <div className="app-wrapper">
       <Header/>
       <Navbar/>
-      <Profile/>
+      <div className="app-wrapper-content">
+        <Router history=''>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/messages' component={Dialogs}/>
+        </Router>
+      </div>
     </div>
+
   );
 }
 
