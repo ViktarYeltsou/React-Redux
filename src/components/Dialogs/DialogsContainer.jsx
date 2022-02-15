920f81cdec1a5b4cfe19a26b169cec05364e7ea8
ViktarYeltsou <fhctybl.33>
2022-02-15 12:03:39 +0300

    add: DialogsContainer

---
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,18 @@
+import {addMessageAC, updateNewMessageAC} from "../../redux/dialogsReducer";
+import Dialogs from "./Dialogs";
+
+const DialogsContainer = (props) => {
+
+  const state = props.store.getState().dialogPage;
+
+  const addMessage = () => {
+    props.store.dispatch(addMessageAC())
+  }
+  const onMessageChange = (body) => {
+    props.store.dispatch(updateNewMessageAC(body));
+  }
+
+  return <Dialogs addMessage={addMessage} updateNewMessageText={onMessageChange} state={state}/>
+
+}
+export default DialogsContainer;
