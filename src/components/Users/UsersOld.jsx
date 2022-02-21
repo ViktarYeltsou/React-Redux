f1eb6b5bd1905bae2cfe4f8436605c80bfb386ef
ViktarYeltsou <fhctybl.33>
2022-02-21 22:41:01 +0300

    refactor: change User component with class User component (53)

--- src/components/Users/UsersOld.jsx
+++ src/components/Users/UsersOld.jsx
@@ -1,32 +1,32 @@
 import classes from "./Users.module.css";
 import axios from "axios";
-import React from "react";
 
-class UsersC extends React.Component {
-  constructor(props) {
-    super(props);
+const Users = (props) => {
+  if (props.users.length === 0) {
+
+    // const axios = require('axios');
+
     axios.get("http://localhost:5000/api/users").then(response => {
       console.log(response)
-      this.props.setUsers(response.data)
-    });
+      props.setUsers(response.data)
+    })
   }
 
-  render() {
-    return <div>
-      {
-        this.props.users.map(u =>
-          <div key={u.id}>
+  return <div>
+    {
+      props.users.map(u =>
+        <div key={u.id}>
           <span>
             <div>
               <img src={u.userUrl} alt="ava" className={classes.userPhoto}/>
             </div>
 
             {u.followed
-              ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
-              : <button onClick={() => this.props.follow(u.id)}>Follow</button>
+              ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+              : <button onClick={() => props.follow(u.id)}>Follow</button>
             }
           </span>
-            <span>
+         <span>
            <span>
              <div>{u.userName}</div>
              <div>{u.userStatus}</div>
@@ -36,13 +36,13 @@ class UsersC extends React.Component {
              <div>{u.location.country}</div>
            </span>
          </span>
-            <p>___</p>
+          <p>___</p>
 
-          </div>
-        )
-      }
+        </div>
+      )
+    }
 
-    </div>
-  }
+  </div>
 }
-export default UsersC;
+
+export default Users;
